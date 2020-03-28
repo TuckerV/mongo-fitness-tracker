303b1583ea5da28a6bba1ea79ee9d071489a5059
Tucker Vassau <tuckv@hotmail.com>
2020-03-28 10:20:18 -0600

    Api routes functioning

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,27 @@
 // API Routing
+const db = require('../models');
 
 module.exports = function (app) {
-
+    app.get('/api/workouts', function (req, res) {
+        db.Workout.find({}).then(function (dbWorkouts){
+            res.json(dbWorkouts)
+        })
+    })
+    app.get('/api/workouts/range', function (req, res) {
+        db.Workout.find({}).then(function (dbWorkouts) {
+            res.json(dbWorkouts)
+        })
+    })
+    app.put('/api/workouts/:id', function (req, res) {
+        db.Workout.update({ _id: req.params.id }, {$push: { 'exercises': req.body } }).then(function (dbWorkouts) {
+            res.json(dbWorkouts)
+        })
+    })
+    app.post('/api/workouts', function(req, res) {
+        db.Workout.create(req.body).then(function (dbWorkouts) {
+            res.json(dbWorkouts)
+        }).catch(err => {
+            res.status(400).json(err);
+        })
+    })
 };
